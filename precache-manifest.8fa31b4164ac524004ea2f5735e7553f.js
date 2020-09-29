d81ab7c1ecf3538b884e5a4b5b879d7c9b0af663
sunhpark42 <sunhpark42@gmail.com>
2020-09-29 17:41:41 +0900

    Updates

---
+++ precache-manifest.8fa31b4164ac524004ea2f5735e7553f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3170dc52cc7884ae4b3a8ff858069205",
+    "url": "/soragodong/index.html"
+  },
+  {
+    "revision": "74e87c91f671b43a4f5e",
+    "url": "/soragodong/static/css/main.0e08f4a4.chunk.css"
+  },
+  {
+    "revision": "2794cb5279d3488ecd48",
+    "url": "/soragodong/static/js/2.66ae6155.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/soragodong/static/js/2.66ae6155.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74e87c91f671b43a4f5e",
+    "url": "/soragodong/static/js/main.fb069597.chunk.js"
+  },
+  {
+    "revision": "c412ce1d4572c49a34dc",
+    "url": "/soragodong/static/js/runtime-main.f829fd22.js"
+  }
+]);
